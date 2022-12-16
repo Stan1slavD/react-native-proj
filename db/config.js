32183a4e9f915d2742c518aeb3845f6148f8e047
Stanislav <stasdamaratskiy@gmail.com>
2022-12-17 01:40:34 +0300

    edit add data to firebase

--- db/config.js
+++ db/config.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(app);
+// export const analytics = getAnalytics(app);
 export const db=getFirestore();
 
 // const firestore = firebase.firestore()
--- screens/search.jsx
+++ screens/search.jsx
@@ -1,11 +1,33 @@
-import React from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList, StyleSheet, Text, View, Button} from "react-native";
 import Card from "../components/card";
-
+import {collection, getDocs, addDoc} from "firebase/firestore"
+import { db } from "../db/config";
 function SearchScreen() {
+  const [flatData, setFlatData]=useState()
+  const[count,setCount]=useState(0)
+  const flatCollectionRef=collection(db,"flats");
+  const requestCollectionRef=collection(db,"requests");
+  const [loaded, setLoaded] = useState(false)
+//   useEffect(()=>{
+//     if (loaded) return ;
+//     setData(getDocs(flatCollectionRef));
+//     console.log(dataa.docs)
+//     setLoaded(true)
+// }, [loaded])
+useEffect(() => {
+  const getUsers = async () => {
+    const data = await getDocs(flatCollectionRef);
+    setFlatData(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    console.log(flatData)
+  };
+
+  getUsers();
+}, []);
   return (
     <View >
       <Text>tut budet filter</Text>
+      <Button title="1" onPress={()=>{console.log(flatData)}}></Button>
       <FlatList
         data={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
         renderItem={({ item }) => <Card/>}
